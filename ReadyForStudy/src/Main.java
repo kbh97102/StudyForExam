ae4a93335c607fabe8a6754728a4ae6ba9ab9257
Arakene <kbh97102@naver.com>
2019-12-13 20:13:20 +0900

    update wakeUp method in Class_2

---
+++ ReadyForStudy/src/Main.java
@@ -0,0 +1,9 @@
+import test_1.Class_1;
+import test_1.Class_2;
+
+public class Main {
+    public static void main(String[] args) {
+        Class_1.wakeup();
+        Class_2.wakeup();
+    }
+}
--- ReadyForStudy/src/test_1/Class_2.java
+++ ReadyForStudy/src/test_1/Class_2.java
@@ -1,5 +1,8 @@
 package test_1;
 
 public class Class_2 extends Class_1 {
-    
+
+    public static void wakeup(){
+        System.out.println("already wake up");
+    }
 }
