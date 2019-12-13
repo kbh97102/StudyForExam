5df2c3a783e13798eb9a8ada3ced5189899997da
Arakene <kbh97102@naver.com>
2019-12-13 20:09:35 +0900

    Add childClass Class_2 (extends Class_1)

--- ReadyForStudy/src/test.java
+++
@@ -1,7 +0,0 @@
-import test_1.Class_1;
-
-public class test {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ ReadyForStudy/src/test_1/Class_2.java
@@ -0,0 +1,5 @@
+package test_1;
+
+public class Class_2 extends Class_1 {
+    
+}
