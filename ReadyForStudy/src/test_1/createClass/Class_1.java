7dfea5b441ef36115031504f94f61215bf30c9b2
Arakene <kbh97102@naver.com>
2019-12-13 19:42:32 +0900

    Add 2 constructors

--- ReadyForStudy/src/test_1/createClass/Class_1.java
+++ ReadyForStudy/src/test_1/createClass/Class_1.java
@@ -4,4 +4,14 @@ public class Class_1 {
     private String name;
     private int age;
     private boolean hasChild;
+    public Class_1(String name, int age, boolean hasChild){
+        this.name = name;
+        this.age = age;
+        this.hasChild = hasChild;
+    }
+    public Class_1(){
+        name = "Defalut Name";
+        age = 23;
+        hasChild = false;
+    }
 }
