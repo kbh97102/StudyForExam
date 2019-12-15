58e5f7867bab25ed373d3da0848d62648e31cc36
Arakene <kbh97102@naver.com>
2019-12-15 19:16:07 +0900

    Add remove(Data) to DynamicArray

--- ReadyForStudy/src/Main.java
+++ ReadyForStudy/src/Main.java
@@ -1,5 +1,23 @@
+import array.DynamicArray;
+
 public class Main {
     public static void main(String[] args) {
-        
+        DynamicArray<Double> testArray = new DynamicArray<>(Double.class);
+        for(int i=0;i<10;i++){
+            testArray.put(i+0.5);
+        }
+        testArray.extend();
+        System.out.println("size"+testArray.size());
+        testArray.put(55.5);
+        System.out.println("size"+testArray.size());
+        for(int i=0;i<testArray.size();i++){
+            System.out.println(testArray.get(i));
+        }
+        testArray.remove(2);
+
+
+        for(int i=0;i<testArray.size();i++){
+            System.out.println(testArray.get(i));
+        }
     }
 }
--- ReadyForStudy/src/array/DynamicArray.java
+++ ReadyForStudy/src/array/DynamicArray.java
@@ -31,15 +31,53 @@ public class DynamicArray<T>{
     @SuppressWarnings("unchecked")
     public void extend(){
 //        T[] temp = dynamicArray;
-        T[] temp = (T[])Array.newInstance(type,defaultSize);
-        if (size >= 0) System.arraycopy(dynamicArray, 0, temp, 0, size);
-        dynamicArray = (T[])Array.newInstance(type,size*2);
-        if (size >= 0) System.arraycopy(temp, 0, dynamicArray, 0, size);
-    }
+        try{
+            T[] temp = (T[])Array.newInstance(type,defaultSize);
+            if (size >= 0) System.arraycopy(dynamicArray, 0, temp, 0, size);
+            dynamicArray = (T[])Array.newInstance(type,size*2);
+            if (size >= 0) System.arraycopy(temp, 0, dynamicArray, 0, size);
+        }
+        catch (IndexOutOfBoundsException e1){
+            System.out.println("Index Error");
+            e1.printStackTrace();
+        }
+        catch (ArrayStoreException e2){
+            System.out.println("Type mismatch");
+            e2.printStackTrace();
+        }
+        catch (NullPointerException e3){
+            System.out.println("Friendly NullPointerException");
+            e3.printStackTrace();
+        }
 
-    public void remove(int index){
+    }
+    public int contains(T data){
+        int index = -1;
+        if(type != String.class){
+            String arrayData;
+            String compareData = data.toString();
+            for(int i=0;i<size;i++){
+                arrayData = dynamicArray[i].toString();
+                if(compareData.equals(arrayData)){
+                    index = i;
+                    break;
+                }
+            }
+        }
+        else{
+            for(int i=0;i<size;i++){
+                if(data.equals(dynamicArray[i])){
+                    index = i;
+                    break;
+                }
+            }
+        }
+        return index;
+    }
+    public void remove(T data){
         try{
             if(size > 0){
+                int index = contains(data);
                 for(int i=index;i<size-1;i++){
                     dynamicArray[i] = dynamicArray[i+1];
                 }
@@ -49,4 +87,17 @@ public class DynamicArray<T>{
             System.out.println("Can't remove, No data in Array");
         }
     }
+    public void remove(int index){
+        try{
+            if(size > 0){
+                if (size - 1 - index >= 0)
+                    System.arraycopy(dynamicArray, index + 1, dynamicArray, index, size - 1 - index);
+                size--;
+            }
+        }catch(IndexOutOfBoundsException e1){
+            System.out.println("Can't remove, No data in Array");
+        }
+    }
+
+    //TODO check is Running and Improve performance
 }
