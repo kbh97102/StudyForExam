b8a39b3ad9e1a42ce32b19b421848d2c8559e6b4
Arakene <kbh97102@naver.com>
2019-12-15 18:39:56 +0900

    Add extend to DynamicArray

--- ReadyForStudy/src/array/DynamicArray.java
+++ ReadyForStudy/src/array/DynamicArray.java
@@ -8,10 +8,11 @@ public class DynamicArray<T>{
     private int resizeDegree;
 //    private Object[] arr;
     private int defaultSize = 10;
-
+    private Class<T> type;
 
     @SuppressWarnings("unchecked")
     public DynamicArray(Class<T> type){
+        this.type = type;
         size = 0;
         resizeDegree = 2;
 //        arr = new Object[defaultSize];
@@ -26,5 +27,13 @@ public class DynamicArray<T>{
     }
     public int size(){
         return size;
+    }
+    @SuppressWarnings("unchecked")
+    public void extend(){
+//        T[] temp = dynamicArray;
+        T[] temp = (T[])Array.newInstance(type,defaultSize);
+        if (size >= 0) System.arraycopy(dynamicArray, 0, temp, 0, size);
+        dynamicArray = (T[])Array.newInstance(type,size*2);
+        if (size >= 0) System.arraycopy(temp, 0, dynamicArray, 0, size);
     }
 }
